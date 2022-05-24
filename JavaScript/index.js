dc9cddf449f896ed2460d6f38e2fa05a440d44a7
newsparkly <dastonbekofficial@gmail.com>
2022-05-24 18:10:42 +0500

    after renaming auto update activated

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -70,6 +70,7 @@ function handleAddTodo (evt) {
 
 let editerBtn = fEl('.edit');
 function editTodoItem (evt) {
+    let filteredList = [];
     for (let i = 0; i < toDoItem.length; i++) {
         if (toDoItem[i].id == evt.target.dataset.id){
             let editedValue = prompt('Edit the todo!', toDoItem[i].title);
@@ -77,11 +78,13 @@ function editTodoItem (evt) {
                 return alert(`You have not entered a name!`);
             } else {
                 toDoItem[i].title = editedValue;
+                filteredList.push(toDoItem[i]);
             }
             window.localStorage.clear;
             window.localStorage.setItem('todoslist', JSON.stringify(toDoItem));
         }
     }        
+    render(filteredList)
 }
 
 list.addEventListener('click', (evt) => {
